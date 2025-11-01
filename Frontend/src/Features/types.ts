4b2a7091fdb97aef66a151b428ee5aa3ccc57126
Seathiel <ogoudedjimonde@gmail.com>
2025-11-01 17:01:56 +0100

    Création des contrats de types via zod pour la validation auth

---
+++ Frontend/src/Features/types.ts
@@ -0,0 +1,19 @@
+import z  from "zod/v4";
+
+const LoginSchema = z.object({
+    email: z.email(),
+    password: z.string().min(8, "Incorrect Password!")
+})
+export type LoginInputs = z.infer<typeof LoginSchema>;
+
+const RegisterSchema = z.object({
+    username: z.string().min(2, "Enter a valid username!"),
+    email: z.email(),
+    password: z.string().min(8, "Minimum 8 characters!").refine(pass => pass.includes('')),
+    confirmPassword: z.string()
+}).refine(schema => schema.password === schema.confirmPassword, {
+    error: "Passwords don't match!",
+    path:['confirmPassword'],
+    when: (payload) => RegisterSchema.pick({password: true, confirmPassword: true}).safeParse(payload.value).success 
+})
+export type RegisterInputs = z.infer<typeof RegisterSchema>;
